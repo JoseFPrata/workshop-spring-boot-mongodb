ae28a7399143fea43c6e54b197f999eb6f56174f
JoseFPrata <ferre22009@hotmail.com>
2022-06-19 19:06:20 -0300

    Criando entity Post com User aninhado, PORÉM MÉTODO SAVE NÃO FUNCIONOU PORISSO NÃO CRIOU. MELHOR FAZER COM DTO

--- src/main/java/com/prata/workshopmongodb/config/Instatiation.java
+++ src/main/java/com/prata/workshopmongodb/config/Instatiation.java
@@ -1,30 +1,45 @@
 package com.prata.workshopmongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
+import com.prata.workshopmongodb.domain.Post;
 import com.prata.workshopmongodb.domain.User;
+import com.prata.workshopmongodb.repository.PostRepository;
 import com.prata.workshopmongodb.repository.UserRepository;
 
 public class Instatiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userReposiroty;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userReposiroty.deleteAll();
+		postRepository.deleteAll();
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo!abs",maria);
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo!abs",maria);
 		
-	}
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
+		
 	
 	
+	}	
 
 }
